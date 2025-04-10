933487281eff8708bca557eca781a60001bde097
Manish <acharyamanish22@gmail.com>
2025-04-10 19:02:14 +0545

    Enable django admin for user proile model

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_ramework.views import APIView
+from rest_ramework.views import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """test api view"""
+
+    def get(self, request, format = None):
+        """returns a list of Apiview features"""
+        an_apiview=[
+             'Uses http methods as function (get ,post, put, patch ,delete)'
+             'Is similar to a traditional django view'
+             "Gives you the most control over your application logic"
+             'is mapped manually to urls'
+        ]
+        return Response({'message':'Hello','an_apiview':an_apiview})
