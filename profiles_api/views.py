1dd22e8453f6ce4563cfd63cf8bcb4a1e2e566b3
Manish <acharyamanish22@gmail.com>
2025-04-30 17:28:02 +0545

    Added Hello-api-view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,5 +1,5 @@
-from rest_ramework.views import APIView
-from rest_ramework.views import Response
+from rest_framework.views import APIView
+from rest_framework.response  import Response
 
 
 class HelloApiView(APIView):
@@ -8,9 +8,9 @@ class HelloApiView(APIView):
     def get(self, request, format = None):
         """returns a list of Apiview features"""
         an_apiview=[
-             'Uses http methods as function (get ,post, put, patch ,delete)'
-             'Is similar to a traditional django view'
-             "Gives you the most control over your application logic"
-             'is mapped manually to urls'
+             'Uses http methods as function (get ,post, put, patch ,delete)',
+             'Is similar to a traditional django view',
+             "Gives you the most control over your application logic",
+             'is mapped manually to urls',
         ]
         return Response({'message':'Hello','an_apiview':an_apiview})
